86592cac5247ae52823bbf41762c77c1085a88c3
V. Oz <v-oz@users.noreply.github.com>
2016-04-19 18:01:36 +0300

    Комментарии в ядро (начало)

--- cmd.core.js
+++ cmd.core.js
@@ -1,18 +1,21 @@
 // supplied AS IS
-// For backtrack and comments: v.ozzzzzzz@gmail.com
+// 
 // command parcer core
 
+// атом системы - команда
 function c(keyword){
 return {
-	keyword:keyword,
-	check: function(s){ re = new RegExp(this.keyword, 'igm'); return re.test(s) },
-	action: function(s){flash(this.keyword+': '+s) }
+	keyword:keyword, // ключевое слово или набор слов через ИЛИ (RegExp) задается в виде строки при инициализации системы
+	check: function(s){ re = new RegExp(this.keyword, 'igm'); return re.test(s) }, // проверка наличия в тексте ключевого слова
+	action: function(s){flash(this.keyword+': '+s) } // выполняемая команда (переопределяется при инициализации системы)
 	}
 }
 
+// молекула системы - набор команд
+// но структура набора команд наследует структуру единичной команды, переопределяя её элементы
 function cSet(keyword){
-var set = c(keyword)
-// override inherited command's action
+var set = c(keyword) // набор = команда
+// действие зачаточного набора переопределяется - 
 	set.action = function(s){
 		if(set.checkSet(s)){
 			set.find(s)
